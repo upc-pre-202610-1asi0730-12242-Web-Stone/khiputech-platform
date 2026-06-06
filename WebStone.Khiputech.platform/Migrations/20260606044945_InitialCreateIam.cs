2d316999be0930ab241496464069914249a64475
luis_ <perghormaru@gmail.com>
2026-06-06 09:01:46 -0500

    feat(shared): implement core architecture including iam and analytics bounded contexts

---
+++ WebStone.Khiputech.platform/Migrations/20260606044945_InitialCreateIam.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebStone.Khiputech.platform.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreateIam : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    username = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    password_hash = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    type = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: false, defaultValue: "public")
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    permissions = table.Column<string>(type: "varchar(200)", maxLength: 200, nullable: false, defaultValue: "")
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    created_at = table.Column<DateTimeOffset>(type: "datetime(6)", nullable: true),
+                    updated_at = table.Column<DateTimeOffset>(type: "datetime(6)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
